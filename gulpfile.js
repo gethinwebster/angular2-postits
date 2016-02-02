280181cde8d5663197561ca794b6d878c2a32123
Gethin Webster <gethin.webster@agilityworks.co.uk>
2016-02-02 09:33:26 +0000

    Add foundation framework

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,13 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     traceur = require('gulp-traceur'),
     webserver = require('gulp-webserver');
+var $ = require('gulp-load-plugins')();
 
 // run init tasks
-gulp.task('default', ['dependencies', 'js', 'html', 'css']);
+gulp.task('default', ['dependencies', 'foundation-deps', 'js', 'html', 'sass']);
 
 // run development task
-gulp.task('dev', ['watch', 'serve']);
+gulp.task('dev', ['dependencies', 'foundation-deps', 'watch', 'serve']);
 
 // serve the build dir
 gulp.task('serve', function () {
@@ -21,7 +22,7 @@ gulp.task('serve', function () {
 gulp.task('watch', function () {
   gulp.watch('src/**/*.js', ['js']);
   gulp.watch('src/**/*.html', ['html']);
-  gulp.watch('src/**/*.css', ['css']);
+  gulp.watch('src/**/*.scss', ['sass']);
 });
 
 // move dependencies into build dir
@@ -32,6 +33,15 @@ gulp.task('dependencies', function () {
     'node_modules/systemjs/dist/system.js',
     'node_modules/reflect-metadata/Reflect.js',
     'node_modules/angular2/bundles/angular2.js'
+  ])
+    .pipe(gulp.dest('build/lib'));
+});
+
+gulp.task('foundation-deps', function () {
+  return gulp.src([
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/what-input/what-input.js',
+    'bower_components/foundation-sites/dist/foundation.js'
   ])
     .pipe(gulp.dest('build/lib'));
 });
@@ -59,6 +69,24 @@ gulp.task('js', function () {
 gulp.task('html', function () {
   return gulp.src('src/**/*.html')
     .pipe(gulp.dest('build'))
+});
+
+var sassPaths = [
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src'
+];
+
+// compile scss
+gulp.task('sass', function() {
+  return gulp.src('src/scss/app.scss')
+    .pipe($.sass({
+      includePaths: sassPaths
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('build/css'));
 });
 
 // move css
